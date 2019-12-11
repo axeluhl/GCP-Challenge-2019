e8a2a457f680a52eba8e52a5b764d4124c8c82b3
Axel Uhl <axel.uhl@sap.com>
2019-12-11 11:04:50 +0100

    added a second (partial) receipt plus test frame

--- static-classification/classifier.js
+++ static-classification/classifier.js
@@ -7,7 +7,9 @@ const {PubSub} = require('@google-cloud/pubsub');
  */
 exports.static_classifier = async (event, context) => {
     console.log("Received message: "+JSON.stringify(event))
-    let data = JSON.parse(Buffer.from(event.data, 'base64'));
+    let payload = Buffer.from(event.data, 'base64');
+    console.log("Base64-decoded payload: "+payload);
+    let data = JSON.parse(payload);
     let store = data.store;
     let items = data.items;
     if (data.store && data.items) {
@@ -25,16 +27,37 @@ exports.static_classifier = async (event, context) => {
 
 function tryToClassify(item) {
   let name = item.name;
-  if (name.match(/Bambusstab/i)) {
-    item.category = "Gärtnereibedarf";
-    item.categoryconfidence = 0.5;
-  } else if (name.match(/Sprüher/i)) {
-    item.category = "Bastelbedarf";
-    item.categoryconfidence = 0.5;
-  } else if (name.match(/Spanplattenschrauben/i)) {
-    item.category = "Eisenwaren";
-    item.categoryconfidence = 0.5;
-  } else {
+  item.categoryconfidence = 0.5;
+  if (name.match(/Bambusstab/i)) item.category = "Gärtnereibedarf";
+  else if (name.match(/Sprüher/i)) item.category = "Bastelbedarf";
+  else if (name.match(/Spanplattenschrauben/i)) item.category = "Eisenwaren";
+  else if (name.match(/Landfrucht Säfte/i)) item.category = "Getränke";
+  else if (name.match(/Traubendirektsaft/i)) item.category = "Getränke";
+  else if (name.match(/Mehrfruchtdirektsa/i)) item.category = "Getränke";
+  else if (name.match(/Karotten/i)) item.category = "Lebensmittel";
+  else if (name.match(/Joghurt/i)) item.category = "Lebensmittel";
+  else if (name.match(/Zahncreme/i)) item.category = "Hygiene";
+  else if (name.match(/Zahnbürste/i)) item.category = "Hygiene";
+  else if (name.match(/Puderzucker/i)) item.category = "Lebensmittel";
+  else if (name.match(/Moser Roth Gefüllt/i)) item.category = "Süßigkeiten";
+  else if (name.match(/Haselnusskerne/i)) item.category = "Lebensmittel";
+  else if (name.match(/Walnusskerne/i)) item.category = "Lebensmittel";
+  else if (name.match(/Romarispen/i)) item.category = "Lebensmittel";
+  else if (name.match(/Oreo/i)) item.category = "Süßigkeiten";
+  else if (name.match(/Nimm 2/i)) item.category = "Süßigkeiten";
+  else if (name.match(/Bad-\/Küchen\/Dusch/i)) item.category = "Hygiene";
+  else if (name.match(/salat/i)) item.category = "Lebensmittel";
+  else if (name.match(/Reinig/i)) item.category = "Haushalt";
+  else if (name.match(/Eier/i)) item.category = "Lebensmittel";
+  else if (name.match(/Bourbon Vanille/i)) item.category = "Lebensmittel";
+  else if (name.match(/Fischstäbchen/i)) item.category = "Lebensmittel";
+  else if (name.match(/Nuss-Frucht-Mix/i)) item.category = "Lebensmittel";
+  else if (name.match(/Multi Power Reinig/i)) item.category = "Haushalt";
+  else if (name.match(/Milchreis/i)) item.category = "Lebensmittel";
+  else if (name.match(/käse/i)) item.category = "Lebensmittel";
+  else if (name.match(/Snack/i)) item.category = "Süßigkeiten";
+  else if (name.match(/Milch/i)) item.category = "Getränke";
+  else {
     item.category = "Unknown";
     item.categoryconfidence = 0.0;
   }
