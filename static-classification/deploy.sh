2000835de3b2be226a6eddce1293530124e1945b
Axel Uhl <axel.uhl@sap.com>
2019-12-11 10:31:02 +0100

    use classifier-account again as the service account; it now has the permissions required

--- static-classification/deploy.sh
+++ static-classification/deploy.sh
@@ -2,3 +2,4 @@ gcloud functions deploy static_classifier --region=europe-west2 \
   --project hackathon-sap19-wal-1009 \
   --runtime nodejs8 \
   --trigger-topic ocr_result
+  --service-account classifier-account@hackathon-sap19-wal-1009.iam.gserviceaccount.com
